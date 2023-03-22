fe8c4f113656b4f559495f08aa9d5d1756b8536e
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-03-22 16:54:26 -0600

    Week 6 Wednesday Lab

---
+++ Frontend/index.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector('button')
+
+const clickHandler = (event) => {
+    event.preventDefault()
+    const messages = [
+        'The worst thing about prison was the dementors.', 
+        'Im not superstitious but I am a little stitious.', 
+        'Mmm. Sort of an oaky afterbirth.', 
+        'I declare bankruptcy!', 
+        'Friends joke with one another. Hey, you are poor. Hey, your mommas dead. Thats what friends do.', 
+        'Thats what she said!'
+    ]
+    const randomIndex = Math.floor(Math.random() * messages.length - 1)
+    const randomMessage = messages[randomIndex]
+    alert(randomMessage)
+    
+}
+
+btn.addEventListener('click', clickHandler)
+
+const uselessBtn = document.getElementById('uselessbutton')
+const uselessClick = () => alert('this button does nothing, its upset about it.')
+console.log(uselessBtn)
+uselessBtn.addEventListener('click', uselessClick)
