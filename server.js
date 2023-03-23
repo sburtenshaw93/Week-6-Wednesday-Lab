bf3a0dd4e635fdd5cb8b7b2a53fcbb38f03901cb
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-03-23 16:28:32 -0600

    Week 6 Thusday lab rollbar

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.use(express.json())
 
 app.use(express.static(`${__dirname}/public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'c5b1d692dff142f79465b5fea92b5d76',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.listen(4000, () => {
     console.log('Server up and running on 4000')
 })
